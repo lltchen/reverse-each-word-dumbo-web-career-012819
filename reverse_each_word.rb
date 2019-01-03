e5c03bb6aca69cd90710cafe500720ec10addfce
lltchen <lltchen1@gmail.com>
2019-01-03 04:13:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sent)
   sentence_reversed = sent.split
   sentence_reversed.collect do |word|
-    return word.reverse
+     word.reverse
   end
+  sentence_reversed = sentence_reversed.join(" ")
 end
